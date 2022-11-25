83e45aa8656e38ee9b6a3fafced1309e6043db63
José Neto <ribeirojoseph44@gmail.com>
2022-11-25 16:40:16 -0300

    Colocando o Websocket

---
+++ server/src/http.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import path from "path";
+import { router } from "./routes/routes";
+import { Server } from "socket.io";
+
+const app = express();
+
+// Configurando o CORS --------------------------------------------------------
+// Adicionando a lista de IP's permitidos a acessar a API.
+const allowedOrigins = ["http://localhost:53264/"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/files", express.static(path.resolve(__dirname, '..','uploads')))
+
+app.use(router);
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp);
+export { serverHttp, io };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,41 @@
-import express from "express";
-import { router } from "./routes/routes";
-import cors from "cors";
-import path from "path";
-
-const app = express();
-
-// Configurando o CORS --------------------------------------------------------
-// Adicionando a lista de IP's permitidos a acessar a API.
-const allowedOrigins = ["http://localhost:53264/"];
-
-const options: cors.CorsOptions = {
-    origin: allowedOrigins
-};
-app.use(cors());
-// app.use(cors(options));
-
-// ---------------------------------------------------------------------------
-
-app.use(express.json());
-
-// Pasta aonde os uploads serão armazenados -----------------------------------
-app.use("/files", express.static(path.resolve(__dirname, '..','uploads')))
-
-app.use(router);
+import { serverHttp } from "./http";
+import "./websocket";
 
 const port = process.env.PORT || 3010
 
-app.listen(port, () => {
+// Como criamos o server http utilizando o express, é possível utilizar o método "listen" do express
+serverHttp.listen(port, () => {
   console.log("Link Start! \u{1F60E} \u{1F44D}")
 })
+
+// import express from "express";
+// import { router } from "./routes/routes";
+// import cors from "cors";
+// import path from "path";
+
+// const app = express();
+
+// // Configurando o CORS --------------------------------------------------------
+// // Adicionando a lista de IP's permitidos a acessar a API.
+// const allowedOrigins = ["http://localhost:53264/"];
+
+// const options: cors.CorsOptions = {
+//     origin: allowedOrigins
+// };
+// app.use(cors());
+// // app.use(cors(options));
+
+// // ---------------------------------------------------------------------------
+
+// app.use(express.json());
+
+// // Pasta aonde os uploads serão armazenados -----------------------------------
+// app.use("/files", express.static(path.resolve(__dirname, '..','uploads')))
+
+// app.use(router);
+
+// const port = process.env.PORT || 3010
+
+// app.listen(port, () => {
+//   console.log("Link Start! \u{1F60E} \u{1F44D}")
+// })
